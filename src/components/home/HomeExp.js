a688f6eeac19a48ebbdcc41d4ef26841e94718cc
Antoine Marionneau <antoine.marionneau@etudiant.univ-lr.fr>
2022-04-23 19:39:20 +0200

    add formation & exp to home

--- src/components/home/HomeExp.js
+++ src/components/home/HomeExp.js
@@ -1,44 +1,44 @@
-import React from 'react';
+import React from 'react'
 
 const expData = [
-    {
-        id: 1,
-        title: 'Stage - AÏOLI DIGITAL',
-        date: 'De Janvier 2021 à Février 2021',
-        descMain: "Lors de mon stage dans l'agence Aïoli Digital j'ai développer une application Web pour la ville des Sables d'Olonne.",
-        desc1: "- Mise en place d'un site Web avec CMS WordPress",
-        desc2: "- TemplateWordPress PHP et Intégration CSS / JavaScript",
-        desc3: "- Utilisation de JavaScript ES6 / Async ",
-    },
-    /*{
-        id: 2,
-        title: 'MERCEDES-SAGA',
-        date: 'De Juillet 2019 à Août 2019',
-        desc: "Manutentionnaire dans le cadre d'un déménagement d’entrepôt chez Mercedes SAGA. J'ai pu y développé une autonomie dans mon travail et un savoir-être en entreprise "
-    },
-    */
+  {
+    id: 1,
+    title: 'Alternant Intégrateur Web - Groupe Dubreuil',
+    date: 'De Septembre 2021 à Septembre 2022',
+    descMain:
+      "J'ai intégré la Holding du Groupe Dubreuil pour y réaliser l'intégration des projets Web des filiales du Groupe Dubreuil",
+    desc1: '- Intégration maquette : Twig/Sass',
+    desc2: '- Templating CMS via Framework Interne PHP',
+    desc3:
+      '- Créer des sites responsive, mobile-first, optimisés et user-friendly',
+  },
+  {
+    id: 2,
+    title: 'Stage - AÏOLI DIGITAL',
+    date: 'De Janvier 2021 à Février 2021',
+    descMain:
+      "Lors de mon stage dans l'agence Aïoli Digital j'ai développer une application Web pour la ville des Sables d'Olonne.",
+    desc1: "- Mise en place d'un site Web avec CMS WordPress",
+    desc2: '- Template WordPress PHP et Intégration CSS / JavaScript',
+    desc3: '- Utilisation de JavaScript ES6 / Async ',
+  },
 ]
 
-
 const HomeExp = () => {
-    return(
-        expData.map((exp) => 
-            {
-                return(
-                    <div className="containerText" key={exp.id}>
-                        <h3 className="expTitle">{exp.title}</h3>
-                        <i className="expDate">{exp.date}</i>
-                        <p className="expDesc">{exp.descMain}</p>
-                        <ul>
-                            <li className="expList">{exp.desc1}</li>
-                            <li className="expList">{exp.desc2}</li>
-                            <li className="expList">{exp.desc3}</li>
-                        </ul>                        
-                    </div>
-                )
-            }
-        ) 
-    );
-};
+  return expData.map((exp) => {
+    return (
+      <div className='containerText' key={exp.id}>
+        <h3 className='expTitle'>{exp.title}</h3>
+        <i className='expDate'>{exp.date}</i>
+        <p className='expDesc'>{exp.descMain}</p>
+        <ul>
+          <li className='expList'>{exp.desc1}</li>
+          <li className='expList'>{exp.desc2}</li>
+          <li className='expList'>{exp.desc3}</li>
+        </ul>
+      </div>
+    )
+  })
+}
 
-export default HomeExp;
+export default HomeExp
--- src/components/home/HomeForma.js
+++ src/components/home/HomeForma.js
@@ -1,47 +1,43 @@
-import React from 'react';
-import { Row } from "react-bootstrap";
+import React from 'react'
+import { Row } from 'react-bootstrap'
 
 const formaData = [
-    {
-        id: 1,
-        title: 'BTS SIO - Option SLAM',
-        date: "De Septembre 2019 à Aujourd'hui",
-        descMain: `​Je suis en cours de formation en BTS SIO option Solutions Logicielles et Applications métiers. J'ai pu assimiler plusieurs notions clés :`,
-        desc1: "- Apprentissage de la Programmation Orienté Objet",
-        desc2: "- Développement Web avec le framework Symfony",
-        desc3: "- Initiation au développement d'applications client Lourd et Mobile",
-    },
-    /*
-    {
-        id: 2,
-        title: "Baccalauréat Scientifique",
-        date: 'De Septembre 2015 à Juillet 2018',
-        desc: "J'ai obtenu mon Bac Scientifique avec Option ISN où j'ai pratiquer mes premières bases d'informatique."
-    
-    },
-    */
+  {
+    id: 1,
+    title: 'LP Informatique - Applications Web',
+    date: 'De Septembre 2021 au 31 août 2022',
+    descMain: `​J'ai eu la chance d'intégrer une licence professionnelle spécialisée dans le développement Web FullStack.`,
+    desc1: '- Développement Back-End : PHP/Symfony',
+    desc2: '- Développement Front-End : React/Angular',
+    desc3: '- Compétence Web Complémentaire : SEO/UX/PWA/WebDesign',
+  },
+  {
+    id: 2,
+    title: 'BTS SIO - Option SLAM',
+    date: 'De Septembre 2019 à Mai 2021',
+    descMain: `​J'ai suivi une formation en BTS SIO option Solutions Logicielles et Applications métiers. J'ai pu assimiler plusieurs notions clés :`,
+    desc1: '- Apprentissage de la Programmation Orienté Objet',
+    desc2: '- Développement Web avec le framework Symfony',
+    desc3:
+      "- Initiation au développement d'applications client Lourd et Mobile",
+  },
 ]
 
 const HomeForma = () => {
-    return(
-        formaData.map((forma) => 
-            {
-                return(
-                    <Row className="containerText" key={forma.id}>
-                        <h3 className="formaTitle">{forma.title}</h3>
-                        <i className="formaDate">{forma.date}</i>
-                        <p className="formaDesc">{forma.descMain}</p>
-                        <ul>
-                            <li className="formaList">{forma.desc1}</li>
-                            <li className="formaList">{forma.desc2}</li>
-                            <li className="formaList">{forma.desc3}</li>
-                        </ul>
+  return formaData.map((forma) => {
+    return (
+      <Row className='containerText' key={forma.id}>
+        <h3 className='formaTitle'>{forma.title}</h3>
+        <i className='formaDate'>{forma.date}</i>
+        <p className='formaDesc'>{forma.descMain}</p>
+        <ul>
+          <li className='formaList'>{forma.desc1}</li>
+          <li className='formaList'>{forma.desc2}</li>
+          <li className='formaList'>{forma.desc3}</li>
+        </ul>
+      </Row>
+    )
+  })
+}
 
-                    </Row>
-                )
-            }
-        ) 
-    );
-};
-
-export default HomeForma;
+export default HomeForma
